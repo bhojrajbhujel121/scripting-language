145c6137a8af9b4351c6829ac1ffd605d4ec38b0
bhojrajbhujel121 <bhujelb121121@gmail.com>
2023-12-08 08:32:00 +0545

    first assignment

---
+++ operator/compare.js
@@ -0,0 +1,18 @@
+let a=3
+let b=6 
+   if (a>b){
+    conosole.log("a is greater than b")
+
+   }else if (a<b)
+   {
+    console.log("a is less than b")
+
+   }else {
+    console.log("a is equal to b")
+   }
+
+   
+   
+   
+   
+
